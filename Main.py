05795b068ea460d2ed586491a06b5dce3b2e3b2c
Kevin Monteith <kmonteith@student.bridgew.edu>
2020-01-29 13:15:17 -0500

    Fixed Formatting

--- Main.py
+++ Main.py
@@ -1,13 +1,16 @@
 import requests
 import json
 
+
 def get_jobs():
     get_url = "https://jobs.github.com/positions.json"
     request = requests.get(get_url)
     jobs_json = request.json()
     return jobs_json
 
+
 def jobs_to_file():
     print("hi")
+
 
 get_jobs()
