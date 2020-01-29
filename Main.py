47c466d7e2ac85b57b8975689e683c742608f368
Kevin Monteith <kmonteith@student.bridgew.edu>
2020-01-29 13:12:20 -0500

    Test Commit

--- Main.py
+++ Main.py
@@ -1,4 +1,13 @@
-function getJobs() {
+import requests
+import json
 
+def get_jobs():
+    get_url = "https://jobs.github.com/positions.json"
+    request = requests.get(get_url)
+    jobs_json = request.json()
+    return jobs_json
 
-}
+def jobs_to_file():
+    print("hi")
+
+get_jobs()
