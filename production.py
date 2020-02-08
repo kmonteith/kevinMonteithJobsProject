022fd406ae975b53959363ca7fa1cba805e9fffe
Kevin Monteith <kmonteith@student.bridgew.edu>
2020-02-08 00:44:01 -0500

    Added insert data into db function

--- production.py
+++ production.py
@@ -4,6 +4,8 @@ import requests
 import json
 import os
 import sqlite3
+from datetime import datetime
+import time
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -35,29 +37,36 @@ def close_db(connection: sqlite3.Connection):
 def create_jobs_table(cursor):
     cursor.execute('''CREATE TABLE IF NOT EXISTS 
     jobs(id TEXT PRIMARY KEY, 
-    type TEXT NOT NULL,
-    url TEXT NOT NULL, 
-    created_at LONG NOT NULL, 
-    company TEXT NOT NULL, 
-    company_url TEXT NOT NULL, 
-    location TEXT NOT NULL, 
-    title TEXT NOT NULL, 
-    description TEXT NOT NULL, 
-    how_to_apply TEXT NOT NULL, 
-    company_logo TEXT NOT NULL)''')
+    type TEXT Default None,
+    url TEXT Default None, 
+    created_at FLOAT Default 0.0, 
+    company TEXT Default None, 
+    company_url TEXT Default None, 
+    location TEXT Default None, 
+    title TEXT Default None, 
+    description TEXT Default None, 
+    how_to_apply TEXT Default None, 
+    company_logo TEXT Default None)''')
 
-def insert_data_to_db():
-    # cursor.execute(f'''INSERT INTO STUDENTS (banner_id, first_name, last_name, gpa, credits)
-    #  VALUES (1001, "John", "Santore", {random.uniform(0.0, 4.0)},
-    # {random.randint(0, 120)})''')
-    print("POP")
+
+def insert_data_to_db(cursor, data):
+    for item in data:
+        # datetime_object = datetime.strptime('Jun 1 2005  1:33PM', '%a %b %d %H: %M:%S ')
+        timestamp = datetime.strptime(item['created_at'], '%a %b %d %H:%M:%S %Z %Y').timestamp()
+        print(item['company_url'])
+        cursor.execute('''INSERT OR REPLACE INTO jobs
+                        (id, type, url, created_at, company, company_url, location, title, description, how_to_apply, company_logo)
+                        VALUES (?,?,?,?,?,?,?,?,?,?,?)''',
+                       (item['id'], item['type'], item['url'], timestamp, item['company'], item['company_url'],
+                        item['location'], item['title'], item['description'], item['how_to_apply'],
+                        item['company_logo']))
 
 
 def jobs_to_db():
     # create db for our jobs
     conn, cursor = open_db("jobs.sqlite")
-    print(type(conn))
     create_jobs_table(cursor)
+    insert_data_to_db(cursor, get_jobs())
     close_db(conn)
 
 
